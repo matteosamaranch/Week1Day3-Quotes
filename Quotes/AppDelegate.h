484cdb8780a8712ee0d0f00fdf2fb1a6dc20ff35
Alexander Valero <AlexanderValero@MacBook-Pro-de-Alexander.local>
2015-09-09 14:21:59 +0200

    Initial Commit

---
+++ Quotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quotes
+//
+//  Created by Alexander Valero on 9/9/15.
+//  Copyright (c) 2015 Alexander Valero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quotes
+//
+//  Created by Alexander Valero on 9/9/15.
+//  Copyright (c) 2015 Alexander Valero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
